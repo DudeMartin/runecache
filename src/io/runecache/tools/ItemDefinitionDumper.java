a144689389b99beacb39fd504950b3dd11ea975a
Martin Tuskevicius <martin.tuskevicius@gmail.com>
2017-04-04 10:00:10 -0400

    Needless directory look-ups removed.

--- src/io/runecache/tools/ItemDefinitionDumper.java
+++ src/io/runecache/tools/ItemDefinitionDumper.java
@@ -4,6 +4,7 @@ import io.runecache.Cache;
 import io.runecache.contents.ItemDefinition;
 
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class ItemDefinitionDumper {
@@ -11,10 +12,11 @@ public class ItemDefinitionDumper {
     public static void main(String[] args) throws Exception {
         System.out.println("Starting...");
         Cache cache = new Cache(Cache.locateBaseDirectory());
-        Files.createDirectories(Paths.get("definitions"));
+        Path directoryPath = Paths.get("definitions");
+        Files.createDirectories(directoryPath);
         ItemDefinition[]  definitions = ItemDefinition.get(cache);
         for (int i = 0; i < definitions.length; i++) {
-            Files.write(Paths.get("definitions", i + ".json"), definitions[i].toString().getBytes());
+            Files.write(directoryPath.resolve(i + ".json"), definitions[i].toString().getBytes());
         }
         System.out.println("Dumped " + definitions.length + " item definitions.");
     }
--- src/io/runecache/tools/SpriteDumper.java
+++ src/io/runecache/tools/SpriteDumper.java
@@ -5,6 +5,7 @@ import io.runecache.contents.Sprite;
 
 import javax.imageio.ImageIO;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class SpriteDumper {
@@ -12,12 +13,13 @@ public class SpriteDumper {
     public static void main(String[] args) throws Exception {
         System.out.println("Starting...");
         Cache cache = new Cache(Cache.locateBaseDirectory());
-        Files.createDirectories(Paths.get("sprites"));
+        Path directoryPath = Paths.get("sprites");
+        Files.createDirectories(directoryPath);
         Sprite[]  sprites = Sprite.get(cache);
         for (int i = 0; i < sprites.length; i++) {
             Sprite sprite = sprites[i];
             for (int j = 0; j < sprite.frames.length; j++) {
-                ImageIO.write(sprite.frames[j], "png", Files.newOutputStream(Paths.get("sprites", i + "_" + j + ".png")));
+                ImageIO.write(sprite.frames[j], "png", Files.newOutputStream(directoryPath.resolve(i + "_" + j + ".png")));
             }
         }
         System.out.println("Dumped " + sprites.length + " sprites.");
